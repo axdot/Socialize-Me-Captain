e2c9de530c79ed5366030a5568cbf6f0b5ad3734
Chris <kimballc3@yahoo.com>
2022-07-04 03:06:21 -0500

    user model updated

--- models/user.js
+++ models/user.js
@@ -1,28 +1,41 @@
 const { Schema, model } = require('mongoose');
 const dateFormat = require('../utils/dateFormat');
 
+var validateEmail = function(email) {
+  var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  return re.test(email)
+};
+
 const UserSchema = new Schema(
   {
     userName: {
-      type: String
+      type: String,
+      required: true,
+      unique: true,
+      trim: true
     },
-    createdBy: {
-      type: String
+    email: {
+      type: String,
+      unique: true,
+      required: 'Email address is required',
+        validate: [validateEmail, 'Please fill a valid email address'],
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
     },
     createdAt: {
       type: Date,
       default: Date.now,
       get: (createdAtVal) => dateFormat(createdAtVal)
     },
-    size: {
-      type: String,
-      default: 'Large'
-    },
-    toppings: [],
     thoughts: [
       {
         type: Schema.Types.ObjectId,
         ref: 'Thought'
+      }
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
       }
     ]
   },
@@ -35,8 +48,8 @@ const UserSchema = new Schema(
   }
 );
 
-UserSchema.virtual('thoughtCount').get(function() {
-  return this.thoughts.reduce((total, thought) => total + thought.replies.length + 1, 0);
+UserSchema.virtual('friendCount').get(function() {
+  return this.friends.reduce((total, friends) => total + friends.replies.length + 1, 0);
 });
 
 // create the User model using the UserSchema
